4af751da4e0afd32a3122d5e935d076caeba6e46
Sai Sandeep Shivalkar <saishivalkar1411@gmail.com>
2026-08-19 20:09:38 +0530

    Add Hello.java with initilal content

--- Hello.java
+++ Hello.java
@@ -3,6 +3,5 @@ package git_assignment_;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Good Morning!");
     }
 }

4d4e2f243d094c6c88be7b2b2ae793f25582f0b8
Sai Sandeep Shivalkar <saishivalkar1411@gmail.com>
2026-08-19 20:10:50 +0530

    Add 'Good Morning!' message to Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ package git_assignment_;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Good Morning!");
     }
 }
